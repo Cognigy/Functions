09cf58b52f0396b249aa8859db276d51ae7a5b7e
Alexander Teusz <teusz.alexander@gmail.com>
2021-05-20 10:21:01 +0200

    initial commit

---
+++ functions/star-wars-api/README.md
@@ -0,0 +1,25 @@
+# The Star Wars API
+
+This function provides an exmaple of how one could call an API using an HTTP request. In this case, the [Star Wars API]() is used in order to retrieve various information:
+
+- People
+- Planets
+- Vehicles
+- ...
+
+## Example Execution
+
+Within a Flow, the [Trigger Function](https://docs.cognigy.com/docs/cognigy-functions-1#triggering-a-function) Node can be used.
+
+<img src="./docs/exampleFlow.PNG" width="400" />
+
+Inside **Parameters** JSON field, additional information can be provided:
+
+```json
+{
+    "path": "people",
+    "index": "1"
+}
+```
+
+Last but not least, one can add a Say Node in the **On Scheduled** path. This will be executed, as soon as the Function returned a result (injected). Therefore, the result will be stored in `{{input.data}}`.
---
+++ functions/star-wars-api/function.ts
@@ -0,0 +1,48 @@
+
+export default async ({ parameters, api }: IFunctionExecutionArguments) => {
+
+    /**
+     * The parameters-object contains the parameters with which
+     * this instances of your Function was started. The
+     * data can be accesed and used for data-exchange with e.g.
+     * your calling Flow.
+     */
+    const { path, index } = parameters;
+    const { sessionId, urlToken, userId } = parameters;
+
+    try {
+        /**
+         * The API can be used in your Function code
+         * in order to perform actions with third-party systems as well
+         * as the current system via its RESTful API.
+         */
+        const response = await api.httpRequest({
+            method: "get",
+            url: `https://swapi.dev/api/${path}/${index}/`,
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+
+        /**
+         * Cleanup the result
+         */
+        delete response.data?.created;
+        delete response.data?.edited;
+
+        /**
+         * Inject the API result into the current ongoing chat conversation
+         */
+        api.inject({
+            sessionId,
+            URLToken: urlToken,
+            userId,
+            text: "",
+            data: response.data
+        });
+
+    } catch (error) {
+        // Do something with the error message
+        // api.inject({});
+    }
+}
